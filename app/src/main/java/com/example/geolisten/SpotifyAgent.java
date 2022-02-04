7bad0d39e1be1267f003c0e5b23938b25c9be63b
yuanningma <yuanning_ma@alumni.brown.edu>
2022-02-03 23:05:31 -0500

    Comments

--- app/src/main/java/com/example/geolisten/SpotifyAgent.java
+++ app/src/main/java/com/example/geolisten/SpotifyAgent.java
@@ -3,6 +3,11 @@ package com.example.geolisten;
 // This will be the only class that directly interacts with Spotify API. We will convert everything here to our custom Context information
 public class SpotifyAgent {
 
+    // Unique identifier for the app. Used for:
+    private String clientId;
+    // Key used to authorize App's Web API or SDK calls. We call get with this as our authorizer; it goes to user who, if SPotify is open, will get the
+    // option to authorize back. If authorized, we get our token :-)
+    private String clientSecret;
 
     public Context retrieveContext(String spotifyId) {
         return null;
